e44909b4162c934e0bbb80e8eaa51bfa0c04e4ef
otimakana <otimakana@otimakanas-MacBook-Pro.local>
2023-08-24 20:21:40 +0700

    first commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Providers;
+
+
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+// use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    public static $permission = [
+        'dashboard'=> ['superadmin', 'admin'],
+        'user-index' => ['admin'],
+        'payment-update'=> ['superadmin'],
+    ];
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        //
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        foreach(self::$permission as $feature => $roles) {
+            Gate::define($feature, function(User $user)use ($roles) {
+                if (in_array($user->role, $roles)){
+                    return true;
+                }
+               });
+        }
+
+
+    //    Gate::define('dashboard', function(User $user){
+    //     if ($user->role == 'admin'){
+    //         return true;
+    //     }
+    //    });
+
+    //    Gate::define('dashboard', function(User $user){
+    //     if ($user->role == 'user'){
+    //         return true;
+    //     }
+    //    });
+    }
+}
+
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(20)->create();
+        User::create([
+            'name' => 'Widya Pratama',
+            'email' => 'otimtamo8812@gmail.com',
+            'email_verified_at' => now(),
+            'role' => 'admin',
+            'phone' => '6282112291200',
+            'bio' => 'chemistry teacher and dev',
+            'password'=> Hash::make('123456'),
+        ]);
+
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'email_verified_at' => now(),
+            'role' => 'superadmin',
+            'phone' => '628561638443',
+            'bio' => 'chemistry teacher and dev',
+            'password'=> Hash::make('123456'),
+        ]);
+    }
+}
